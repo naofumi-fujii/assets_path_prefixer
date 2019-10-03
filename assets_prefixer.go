85f5d62aa6d64fce18e599591091ab725b9b0a62
naofumi-fujii <naofumi.fujii@gmail.com>
2019-10-03 15:51:27 +0900

    ContenType should be text/html; charset=utf-8

--- assets_prefixer.go
+++ assets_prefixer.go
@@ -4,16 +4,57 @@ import (
 	"bytes"
 	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 
 	"golang.org/x/net/html"
 )
 
+func GetFileContentType1(filepath string) string {
+
+	// Open File
+	f, err := os.Open(filepath)
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	// Get the content
+	contentType, err := GetFileContentType(f)
+	if err != nil {
+		panic(err)
+	}
+
+	//fmt.Println("Content Type: " + contentType)
+	return contentType
+}
+
+func GetFileContentType(out *os.File) (string, error) {
+
+	// Only the first 512 bytes are used to sniff the content type.
+	buffer := make([]byte, 512)
+
+	_, err := out.Read(buffer)
+	if err != nil {
+		return "", err
+	}
+
+	// Use the net/http package's handy DectectContentType function. Always returns a valid
+	// content-type by returning "application/octet-stream" if no others seemed to match.
+	contentType := http.DetectContentType(buffer)
+
+	return contentType, nil
+}
+
 func main() {
 	//filepath := "public/adinfo.html"
 	prefix := os.Args[1]
 	filepath := os.Args[2]
+	if GetFileContentType1(filepath) != "text/html; charset=utf-8" {
+		log.Fatal("ContenType should be text/html; charset=utf-8")
+	}
+
 	content, err := ioutil.ReadFile(filepath)
 	if err != nil {
 		log.Fatal(err)
